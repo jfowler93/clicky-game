8edc17f774289be78a3d124b80dd6616e9689d7e
Joshua <fowlerjosh93@gmail.com>
2019-10-30 22:42:18 -0400

    Updates

---
+++ precache-manifest.6d455f35dadd35999c47cefdfe7ab05c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf3beaaa8ee6d9f276c87d69d8ee2e0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fcc7fc3a8bff5c370c80",
+    "url": "/clicky-game/static/css/main.13d9cf8e.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "fcc7fc3a8bff5c370c80",
+    "url": "/clicky-game/static/js/main.9347b189.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
